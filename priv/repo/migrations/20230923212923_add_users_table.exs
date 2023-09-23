f2658a15b52c801dce83a47b88f0a8264d2f6937
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-09-23 18:37:09 -0300

    Add migration for users table

---
+++ priv/repo/migrations/20230923212923_add_users_table.exs
@@ -0,0 +1,15 @@
+defmodule CrosswordsApi.Repo.Migrations.AddUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table("users") do
+      add :name, :string, null: false
+      add :password_hash, :string, null: false
+      add :email, :string, null: false
+
+      timestamps()
+    end
+
+    create unique_index("users", :email)
+  end
+end
