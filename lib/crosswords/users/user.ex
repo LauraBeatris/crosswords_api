f8a123c610d06df17f5c81e092ad1c124498686d
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-10-10 01:17:34 -0300

    Add user schema

---
+++ lib/crosswords/users/user.ex
@@ -0,0 +1,20 @@
+defmodule Crosswords.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :name, :string
+    field :email, :string,
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  def changeset(user, params) do
+    user
+    |> cast(params, [:name, :email, :password_hash])
+    |> validate_required([:name, :email, :password_hash])
+    |> validate_format(:email, ~r/@/)
+    |> unique_constraint(:email)
+  end
+end
