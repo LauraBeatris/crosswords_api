dc7cdf64f7545dfd2f95f2a1340c4e125153cf06
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-02-19 09:09:48 -0300

    Introduce module to verify password

---
+++ lib/crosswords_api/users/authenticate.ex
@@ -0,0 +1,17 @@
+defmodule CrosswordsApi.Users.Authenticate do
+  alias CrosswordsApi.Users
+
+  def call(%{"id" => id, "password" => password}) do
+    case Users.get(id) do
+      {:ok, user} -> verify_password(user, password)
+      {:error, _} = error -> error
+    end
+  end
+
+  defp verify_password(user, password) do
+    case Argon2.verify_pass(password, user.password_hash) do
+      true -> {:ok, :valid_password}
+      false -> {:error, :unauthorized}
+    end
+  end
+end
